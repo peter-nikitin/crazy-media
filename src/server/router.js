ddf4073b594f704c16e46302db09ce98adf80d12
Petr Nikitin <nikitin.p.94@gmail.com>
2019-07-12 22:19:14 +0300

    dalaet mock data

--- src/server/router.js
+++ src/server/router.js
@@ -5,31 +5,8 @@ const router = express.Router();
 
 autoroutes(router);
 
-const footerWords = [
-  'дизайн',
-  'мозг',
-  'информация',
-  'язык',
-  'текст',
-  'код',
-  'документ',
-  'метод',
-  'пустота',
-  'навсегда',
-  'ответственность',
-  'игра',
-  'любовь',
-  'свобода',
-  'интерес',
-  'забота',
-  'синхронизация',
-  'дизайнер',
-  'ниндзя',
-  'я',
-];
-
 router.get('/', (req, res) => {
-  res.render('index', { footerWords });
+  res.render('index');
 });
 
 module.exports = router;
