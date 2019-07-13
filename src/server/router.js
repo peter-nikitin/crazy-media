17669a328cb8b241cbbb955063cf21041d4880e3
Petr Nikitin <nikitin.p.94@gmail.com>
2019-07-13 21:33:03 +0300

    add invoke telegram at index

--- src/server/router.js
+++ src/server/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const autoroutes = require('./internals/autoroutes');
+const telegram = require('./utils/telegram/index.js');
 
 const router = express.Router();
 
 autoroutes(router);
 
 router.get('/', (req, res) => {
+  telegram();
   res.render('index');
 });
 
