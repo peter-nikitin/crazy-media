361910b18ebb16be77958cc25b9e77a72a95e1dc
Petr Nikitin <nikitin.p.94@gmail.com>
2019-07-14 13:17:06 +0300

    move invoke of telegram to main

--- src/server/router.js
+++ src/server/router.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const autoroutes = require('./internals/autoroutes');
-const telegram = require('./utils/telegram/index.js');
 
 const router = express.Router();
 
 autoroutes(router);
 
 router.get('/', (req, res) => {
-  telegram();
   res.render('index');
 });
 
